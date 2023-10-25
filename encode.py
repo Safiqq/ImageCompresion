2c4a73d1a398be3197e41b63a9a4286350b1220a
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-10-25 14:59:03 +0700

    feat: add rectangle box

--- encode.py
+++ encode.py
@@ -11,7 +11,42 @@ scale_height = screen_res[1] / h
 scale = min(scale_width, scale_height)
 
 pixel_size = min(w, h)
-print('start pixelsize', pixel_size)
+
+p1 = [0, 0]
+p2 = [pixel_size, pixel_size]
+
+def on_change_x1(val):
+    global pixel_size, w, h
+    p1[0] = val
+    p2[0] = min(val + pixel_size, w)
+    pixel_size = p2[0] - p1[0]
+    cv2.setTrackbarPos('Pixel size', 'Y Component', pixel_size)
+    p2[1] = p1[1] + pixel_size
+    image_rec = image.copy()
+    cv2.setTrackbarMax('Pixel size', 'Y Component', min(w - p1[0], h - p1[1]))
+    cv2.rectangle(image_rec, p1, p2, (0, 0, 0), 5)
+    cv2.imshow('Y Component', image_rec)
+
+def on_change_y1(val):
+    global pixel_size, w, h
+    p1[1] = val
+    p2[1] = min(val + pixel_size, h)
+    pixel_size = p2[1] - p1[1]
+    cv2.setTrackbarPos('Pixel size', 'Y Component', pixel_size)
+    p2[0] = p1[0] + pixel_size
+    image_rec = image.copy()
+    cv2.setTrackbarMax('Pixel size', 'Y Component', min(w - p1[0], h - p1[1]))
+    cv2.rectangle(image_rec, p1, p2, (0, 0, 0), 5)
+    cv2.imshow('Y Component', image_rec)
+
+def on_change_px(val):
+    global pixel_size
+    p2[0] = p1[0] + val
+    p2[1] = p1[1] + val
+    image_rec = image.copy()
+    cv2.rectangle(image_rec, p1, p2, (0, 0, 0), 5)
+    cv2.imshow('Y Component', image_rec)
+    pixel_size = val
 
 if image is None:
     print("Error: Could not load the image.")
@@ -25,12 +60,17 @@ else:
 
     image = y_channel
     image_rec = image.copy()
+    cv2.rectangle(image_rec, p1, p2, (0, 0, 0), 5)
     cv2.imshow('Y Component', image_rec)
     cv2.imwrite('y.jpg', y_channel)
     cv2.imwrite('cr.jpg', cr_channel)
     cv2.imwrite('cb.jpg', cb_channel)
 
     cv2.imwrite('ycrcb.jpg', ycrcb_image)
+
+    cv2.createTrackbar('X start', 'Y Component', 0, w, on_change_x1)
+    cv2.createTrackbar('Y start', 'Y Component', 0, h, on_change_y1)
+    cv2.createTrackbar('Pixel size', 'Y Component', pixel_size, min(w - p1[0], h - p1[1]), on_change_px)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
